c67c1ff963473191c4878150a4145edb74266d1b
bcorrie <bdcorrie@gmail.com>
2021-04-26 21:44:49 +0000

    Cleaned up some more bits - made all files more "minimal"

--- apps/hello-world/0.1/cedar/hello-world.sh
+++ apps/hello-world/0.1/cedar/hello-world.sh
@@ -8,45 +8,15 @@
 # These get set by Tapis
 
 # input files
-###singularity_image="${singularity_image}"
-###rearrangement_file="${rearrangement_file}"
+# NONE
 
 # application parameters
-###single_flag=${single_flag}
-###optional_number="${optional_number}"
-###optional_enum="${optional_enum}"
+# NONE
 
 # Agave info
 AGAVE_JOB_ID=${AGAVE_JOB_ID}
 AGAVE_JOB_NAME=${AGAVE_JOB_NAME}
 AGAVE_LOG_NAME=${AGAVE_JOB_NAME}-${AGAVE_JOB_ID}
-
-
-# ----------------------------------------------------------------------------
-# unpack local executables
-###tar zxf binaries.tgz
-
-# modules
-###module load python3
-###module load singularity
-
-###PYTHON=python3
-
-###export PATH="$PWD/bin:${PATH}"
-###export PYTHONPATH=$PWD/lib/python3.7/site-packages:$PYTHONPATH
-
-# bring in common functions
-###source ./toy_common.sh
-
-# ----------------------------------------------------------------------------
-# Launcher to use multicores on node
-###export LAUNCHER_WORKDIR=$PWD
-###export LAUNCHER_LOW_PPN=1
-###export LAUNCHER_MID_PPN=8
-###export LAUNCHER_MAX_PPN=25
-###export LAUNCHER_PPN=1
-###export LAUNCHER_JOB_FILE=joblist
-###export LAUNCHER_SCHED=interleaved
 
 # Start
 printf "START at $(date)\n\n"
@@ -56,18 +26,12 @@ export JOB_ERROR=0
 
 # Say Hello
 echo "Hello World from Tapis"
-###print_parameters
-###print_versions
-###run_toy_workflow
 
-echo "sleeping"
+echo "Sleeping for a couple of minutes so you can see me in the queue..."
 sleep 120
 
 # End
 printf "DONE at $(date)\n\n"
-
-# remove binaries before archiving 
-###rm -rf bin lib
 
 if [[ $JOB_ERROR -eq 1 ]]; then
     ${AGAVE_JOB_CALLBACK_FAILURE}
