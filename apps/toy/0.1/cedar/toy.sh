ccd6ccbc78d00524100c2e93cf57f89e3eac3616
bcorrie <bdcorrie@gmail.com>
2021-04-27 18:58:07 +0000

    Removed extra stuff not required by cedar.

--- apps/toy/0.1/cedar/toy.sh
+++ apps/toy/0.1/cedar/toy.sh
@@ -22,30 +22,12 @@ AGAVE_JOB_NAME=${AGAVE_JOB_NAME}
 AGAVE_LOG_NAME=${AGAVE_JOB_NAME}-${AGAVE_JOB_ID}
 
 # ----------------------------------------------------------------------------
-# unpack local executables
-###tar zxf binaries.tgz
 
 # modules
-#module load python
 module load singularity
-
-#PYTHON=python3
-
-#export PATH="$PWD/bin:${PATH}"
-#export PYTHONPATH=$PWD/lib/python3.7/site-packages:$PYTHONPATH
 
 # bring in common functions
 source ./toy_common.sh
-
-# ----------------------------------------------------------------------------
-# Launcher to use multicores on node
-#export LAUNCHER_WORKDIR=$PWD
-#export LAUNCHER_LOW_PPN=1
-#export LAUNCHER_MID_PPN=8
-#export LAUNCHER_MAX_PPN=25
-#export LAUNCHER_PPN=1
-#export LAUNCHER_JOB_FILE=joblist
-#export LAUNCHER_SCHED=interleaved
 
 # Start
 printf "START at $(date)\n\n"
@@ -59,9 +41,6 @@ run_toy_workflow
 
 # End
 printf "DONE at $(date)\n\n"
-
-# remove binaries before archiving 
-#rm -rf bin lib
 
 if [[ $JOB_ERROR -eq 1 ]]; then
     ${AGAVE_JOB_CALLBACK_FAILURE}
