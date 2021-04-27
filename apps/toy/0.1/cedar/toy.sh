8c3514264e97b279ac64e57c92facd9873910792
bcorrie <bdcorrie@gmail.com>
2021-04-27 16:06:09 +0000

    Initial test verion.

--- apps/toy/0.1/cedar/toy.sh
+++ apps/toy/0.1/cedar/toy.sh
@@ -23,29 +23,29 @@ AGAVE_LOG_NAME=${AGAVE_JOB_NAME}-${AGAVE_JOB_ID}
 
 # ----------------------------------------------------------------------------
 # unpack local executables
-tar zxf binaries.tgz
+###tar zxf binaries.tgz
 
 # modules
-module load python3
+module load python
 module load singularity
 
-PYTHON=python3
+#PYTHON=python3
 
-export PATH="$PWD/bin:${PATH}"
-export PYTHONPATH=$PWD/lib/python3.7/site-packages:$PYTHONPATH
+#export PATH="$PWD/bin:${PATH}"
+#export PYTHONPATH=$PWD/lib/python3.7/site-packages:$PYTHONPATH
 
 # bring in common functions
 source ./toy_common.sh
 
 # ----------------------------------------------------------------------------
 # Launcher to use multicores on node
-export LAUNCHER_WORKDIR=$PWD
-export LAUNCHER_LOW_PPN=1
-export LAUNCHER_MID_PPN=8
-export LAUNCHER_MAX_PPN=25
-export LAUNCHER_PPN=1
-export LAUNCHER_JOB_FILE=joblist
-export LAUNCHER_SCHED=interleaved
+#export LAUNCHER_WORKDIR=$PWD
+#export LAUNCHER_LOW_PPN=1
+#export LAUNCHER_MID_PPN=8
+#export LAUNCHER_MAX_PPN=25
+#export LAUNCHER_PPN=1
+#export LAUNCHER_JOB_FILE=joblist
+#export LAUNCHER_SCHED=interleaved
 
 # Start
 printf "START at $(date)\n\n"
@@ -61,7 +61,7 @@ run_toy_workflow
 printf "DONE at $(date)\n\n"
 
 # remove binaries before archiving 
-rm -rf bin lib
+#rm -rf bin lib
 
 if [[ $JOB_ERROR -eq 1 ]]; then
     ${AGAVE_JOB_CALLBACK_FAILURE}
