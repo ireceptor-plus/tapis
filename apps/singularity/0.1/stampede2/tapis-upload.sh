b95550e9e9c5c9999cd4a09c9bd427b754bcf924
Scott Christley <scott.christley@utsouthwestern.edu>
2021-06-22 11:16:43 -0500

    stampede2 version

---
+++ apps/singularity/0.1/stampede2/tapis-upload.sh
@@ -0,0 +1,32 @@
+#
+TOOL=singularity
+SYSTEM=stampede2
+VER=0.1
+
+# Copy all of the object files to the bundle directory
+# and create a binaries.tgz
+#
+# For example:
+# cd bundle
+
+# tar zcvf binaries.tgz bin lib
+
+# delete old working area in tapis
+tapis files delete agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM
+
+# create directory structure
+tapis files mkdir agave://data.vdjserver.org/irplus/apps $TOOL
+tapis files mkdir agave://data.vdjserver.org/irplus/apps/$TOOL $VER
+tapis files mkdir agave://data.vdjserver.org/irplus/apps/$TOOL/$VER $SYSTEM
+tapis files mkdir agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM test
+
+# upload app assets
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM singularity.sh
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM singularity.json
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../common/singularity_common.sh
+tapis files list agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM
+
+# upload test assets
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM/test test/test.sh
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM/test test/test-sonar.json
+tapis files list agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM/test
