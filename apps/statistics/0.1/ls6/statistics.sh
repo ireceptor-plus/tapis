e2e81780d03b67d7cac2d43d64169e00e0f16047
Scott Christley <scott.christley@utsouthwestern.edu>
2022-02-28 14:58:22 -0600

    statistics app for lonestar6

---
+++ apps/statistics/0.1/ls6/statistics.sh
@@ -0,0 +1,80 @@
+#
+# Tapis app template script for Lonestar6
+# Part of the iReceptor+ platform
+#
+# Author: Scott Christley
+# Copyright (C) 2022 The University of Texas Southwestern Medical Center
+# Date: Feb 28, 2022
+#
+
+# Configuration settings
+
+# These get set by Tapis
+
+# input files
+singularity_image="${singularity_image}"
+repcalc_image="${repcalc_image}"
+germline_db="${germline_db}"
+metadata_file="${metadata_file}"
+airr_tsv_file="${airr_tsv_file}"
+
+# application parameters
+repertoire_id=${repertoire_id}
+file_type=${file_type}
+
+# Agave info
+AGAVE_JOB_ID=${AGAVE_JOB_ID}
+AGAVE_JOB_NAME=${AGAVE_JOB_NAME}
+AGAVE_LOG_NAME=${AGAVE_JOB_NAME}-${AGAVE_JOB_ID}
+
+# ----------------------------------------------------------------------------
+# unpack local executables
+#tar zxf binaries.tgz
+
+#chmod +x gene_usage.R
+#chmod +x aa_properties.R
+chmod +x clonal_abundance.R
+chmod +x diversity_curve.R
+
+# modules
+module load python3/3.9.7
+module load launcher/3.10
+module load tacc-singularity
+
+PYTHON=python3
+
+export PATH="$PWD/bin:${PATH}"
+export PYTHONPATH=$PWD/lib/python3.9/site-packages:$PYTHONPATH
+
+# bring in common functions
+source ./statistics_common.sh
+
+# ----------------------------------------------------------------------------
+# Launcher to use multicores on node
+export LAUNCHER_WORKDIR=$PWD
+export LAUNCHER_LOW_PPN=1
+export LAUNCHER_MID_PPN=8
+export LAUNCHER_MAX_PPN=25
+export LAUNCHER_PPN=1
+export LAUNCHER_JOB_FILE=joblist
+export LAUNCHER_SCHED=interleaved
+
+# Start
+printf "START at $(date)\n\n"
+
+# If you want to tell Tapis that the job failed
+export JOB_ERROR=0
+
+print_parameters
+print_versions
+run_statistics_workflow
+
+# End
+printf "DONE at $(date)\n\n"
+
+# remove binaries before archiving 
+rm -rf bin lib
+
+if [[ $JOB_ERROR -eq 1 ]]; then
+    ${AGAVE_JOB_CALLBACK_FAILURE}
+fi
---
+++ apps/statistics/0.1/ls6/upload-bundle.sh
@@ -0,0 +1,43 @@
+#
+TOOL=statistics
+SYSTEM=ls6
+VER=0.1
+
+# Copy all of the object files to the bundle directory
+# and create a binaries.tgz
+#
+# For example:
+# cd bundle
+
+# tar zcvf binaries.tgz bin lib
+
+# delete old working area in tapis
+tapis files delete agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM
+
+# create directory structure
+tapis files mkdir agave://data.vdjserver.org/irplus/apps $TOOL
+tapis files mkdir agave://data.vdjserver.org/irplus/apps/$TOOL $VER
+tapis files mkdir agave://data.vdjserver.org/irplus/apps/$TOOL/$VER $SYSTEM
+tapis files mkdir agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM test
+
+# upload app assets
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM statistics.sh
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM statistics.json
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/common_functions.sh
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/repcalc_create_config.py
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/junction_length_template.json
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/gene_usage_template.json
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/count_statistics.py
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/clonal_abundance.R
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../../../../vdjserver/common/diversity_curve.R
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../common/statistics_common.sh
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../common/rearrangement_report.py
+#tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../common/gene_usage.R
+#tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM ../common/aa_properties.R
+tapis files list agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM
+
+# upload test assets
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM/test test/test.sh
+tapis files upload agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM/test test/test-app.json
+tapis files list agave://data.vdjserver.org/irplus/apps/$TOOL/$VER/$SYSTEM/test
+
