d0faadd8f074636deb7ed59182072b62d145ea66
Scott Christley <scott.christley@utsouthwestern.edu>
2022-04-04 17:17:26 -0500

    singularity on lonestar6

---
+++ apps/singularity/0.1/ls6/singularity.sh
@@ -0,0 +1,49 @@
+#
+# wrapper script
+# for Lonestar6
+# 
+# Author: Scott Christley
+# Copyright (C) 2021 The University of Texas Southwestern Medical Center
+# Date: Jun 15, 2021
+
+# Configuration settings
+
+# These get set by Tapis
+
+# input files
+
+# application parameters
+docker_image="${docker_image}"
+singularity_image="${singularity_image}"
+
+# Agave info
+AGAVE_JOB_ID=${AGAVE_JOB_ID}
+AGAVE_JOB_NAME=${AGAVE_JOB_NAME}
+AGAVE_LOG_NAME=${AGAVE_JOB_NAME}-${AGAVE_JOB_ID}
+
+# ----------------------------------------------------------------------------
+# modules
+module load tacc-singularity
+
+# bring in common functions
+source ./singularity_common.sh
+
+# Start
+printf "START at $(date)\n\n"
+
+# If you want to tell Tapis that the job failed
+export JOB_ERROR=0
+
+print_parameters
+print_versions
+run_workflow
+
+# End
+printf "DONE at $(date)\n\n"
+
+# remove binaries before archiving 
+rm -rf bin lib
+
+if [[ $JOB_ERROR -eq 1 ]]; then
+    ${AGAVE_JOB_CALLBACK_FAILURE}
+fi
